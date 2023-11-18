6c574ea01c61b0b2f3edb326bb07acb16e366f9d
Elroy Saltzherr <lsaltzherr@gmail.com>
2023-11-17 20:25:25 -0800

    bit of cleanup

--- docs/GravityBird/main.js
+++ docs/GravityBird/main.js
@@ -56,98 +56,98 @@ let pipes;
 let cooldown = 10
 let animationCountdown = cooldown;
 let animationAt = "a";
-// let score = 0;
 
-function update() {
-if (!ticks) {
-bird = { x: G.BIRD_X, y: G.HEIGHT / 2, vy: 0 };
-pipes = [{ x: G.WIDTH, gapY: rnd(30, 70) }];
-score = 0;
-}
 
-bird.vy += G.GRAVITY;
-bird.y += bird.vy;
-
-if (bird.y < 0 || bird.y > G.HEIGHT) {
-end();
-}
-
-if (input.isJustPressed) {
-    G.GRAVITY *= -1
-    if(G.GRAVITY >= 0){
-        bird.vy = -G.FLAP_STRENGTH
-    }
-    else{
-        bird.vy = G.FLAP_STRENGTH;
-    }
-    
-
-    if(animationAt == "a"){
-        animationAt = "c"
-    }
-    else if(animationAt == "b"){
-        animationAt = "d"
-    }
-    else if(animationAt == "c"){
-        animationAt = "a"
-    }
-    else if(animationAt == "d"){
-        animationAt = "b"
+    function update() {
+    if (!ticks) {
+        bird = { x: G.BIRD_X, y: G.HEIGHT / 2, vel: 0 };
+        pipes = [{ x: G.WIDTH, gapY: rnd(30, 70) }];
+        score = 0;
     }
 
+    bird.vel += G.GRAVITY;
+    bird.y += bird.vel;
 
-}
-
-if (bird.y > G.HEIGHT) {
-bird.y = G.HEIGHT;
-}
-
-for (let i = 0; i < pipes.length; i++) {
-const pipe = pipes[i];
-pipe.x -= 1;
-if (pipe.x < -20) {
-    pipes.shift();
-    pipes.push({ x: G.WIDTH, gapY: rnd(30, 70) });
-}
-
-if (bird.x > pipe.x && bird.x < pipe.x + 20) {
-    if (bird.y < pipe.gapY - 15 || bird.y > pipe.gapY + 15) {
-    end();
-    }
-}
-
-if (bird.x == pipe.x + 20) {
-    score += 1;
-}
-}
-
-color("green")
-pipes.forEach((pipe) => {
-rect(pipe.x, 0, 20, pipe.gapY - 20);
-rect(pipe.x, pipe.gapY + 20, 20, G.HEIGHT - pipe.gapY - 20);
-});
-
-if(animationCountdown <= 0){
-    if (animationAt == "a"){
-        animationAt = "b"
-    }
-    else if(animationAt == "b"){
-        animationAt = "a"
-    }
-    else if(animationAt == "c"){
-        animationAt = "d"
-    }
-    else if(animationAt == "d"){
-        animationAt = "c"
+    if (bird.y < 0 || bird.y > G.HEIGHT) {
+        end();      
     }
 
+    if (input.isJustPressed) {
+        G.GRAVITY *= -1
+        if(G.GRAVITY >= 0){
+            bird.vel = -G.FLAP_STRENGTH
+        }
+        else{
+            bird.vel = G.FLAP_STRENGTH;
+        }
+        
+
+        if(animationAt == "a"){
+            animationAt = "c"
+        }
+        else if(animationAt == "b"){
+            animationAt = "d"
+        }
+        else if(animationAt == "c"){
+            animationAt = "a"
+        }
+        else if(animationAt == "d"){
+            animationAt = "b"
+        }
+
+
+    }
+
+    if (bird.y > G.HEIGHT) {
+        bird.y = G.HEIGHT;
+    }
+
+    for (let i = 0; i < pipes.length; i++) {
+        const pipe = pipes[i];
+        pipe.x -= 1;
+        if (pipe.x < -20) {
+            pipes.shift();
+            pipes.push({ x: G.WIDTH, gapY: rnd(30, 70) });
+        }
+
+        if (bird.x > pipe.x && bird.x < pipe.x + 20) {
+            if (bird.y < pipe.gapY - 15 || bird.y > pipe.gapY + 15) {
+            end();
+            }
+        }
+
+        if (bird.x == pipe.x + 20) {
+            score += 1;
+        }
+    }
+
+    color("green")
+    pipes.forEach((pipe) => {
+        rect(pipe.x, 0, 20, pipe.gapY - 20);
+        rect(pipe.x, pipe.gapY + 20, 20, G.HEIGHT - pipe.gapY - 20);
+    });
+
+    if(animationCountdown <= 0){
+        if (animationAt == "a"){
+            animationAt = "b"
+        }
+        else if(animationAt == "b"){
+            animationAt = "a"
+        }
+        else if(animationAt == "c"){
+            animationAt = "d"
+        }
+        else if(animationAt == "d"){
+            animationAt = "c"
+        }
 
 
 
-    animationCountdown = cooldown
-}
-animationCountdown -= 1;
-color("black")
-char(animationAt, bird.x, bird.y);
+
+        animationCountdown = cooldown
+    }
+    animationCountdown -= 1;
+    color("black")
+    char(animationAt, bird.x, bird.y);
 
 }
